7187f96e6927a439485d853e5b5e75ed612ed88c
lion <472780330@qq.com>
2020-07-07 05:53:15 +0800

    dock和k8s配置

--- api/api_manage.go
+++ api/api_manage.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"morefruit/api/apicom"
 	"morefruit/api/rpc"
 	"morefruit/common"
@@ -10,9 +12,15 @@ import (
 )
 
 func main() {
+	var test = flag.String("test", "test_var", "test var ")
+	flag.Parse()
 	// token, _ := apicom.CreateToken("lijun", "123", common.TokenExpireTime)
 	// fmt.Println(token)
 	e := gin.Default()
+	e.GET("/test", func(c *gin.Context) {
+		testStr := fmt.Sprintf("test=%s", *test)
+		c.String(200, testStr)
+	})
 	apicom.AppendRoute(e, &rpc.ShopRoute{})
 	apicom.AppendRoute(e, &rpc.AccountRoute{})
 	apicom.AppendRoute(e, &rpc.OrderRoute{})
@@ -20,6 +28,6 @@ func main() {
 	endless.DefaultReadTimeOut = common.ApiReadTimeOut
 	endless.DefaultWriteTimeOut = common.ApiWriteTimeOut
 	endless.DefaultMaxHeaderBytes = 1 << 20
-	endless.ListenAndServeTLS(common.ApiPort, "server.crt", "server.key", e)
-	//endless.ListenAndServe(common.ApiPort, e)
+	//endless.ListenAndServeTLS(common.ApiPort, "server.crt", "server.key", e)
+	endless.ListenAndServe(common.ApiPort, e)
 }
